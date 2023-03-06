042b64923dfc2e1fb9b485fb35a5ae9eb7b32915
Trin4317 <eg.artourbabaev@gmail.com>
2023-03-06 14:22:43 +0000

    switch to tuple

--- public/app.js
+++ public/app.js
@@ -12,11 +12,14 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
+    // using tuple
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -19,10 +19,14 @@ form.addEventListener('submit', (e: Event) => {
 
   let doc: HasFormatter;
 
+  // using tuple
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, 'end');
